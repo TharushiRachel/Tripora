0e107a35fc42bbd2dece54dbcf4d765860a618b1
Akila7302 <bandara98akila@gmail.com>
2022-08-31 19:58:11 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { Image, StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Dashboard from "./app/screens/Dashboard";
 
-import OnboardNavigator from "./app/navigation/OnboardNavigator";
+// import OnboardNavigator from "./app/navigation/OnboardNavigator";
+import BottomNavigator from "./app/navigation/BottomNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <OnboardNavigator />
+      <BottomNavigator />
     </NavigationContainer>
   );
 }
